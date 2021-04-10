2b0db01a8c0d4f4aea2d120248ffb8e4e99d94ec
krburns <kufreabia@gmail.com>
2021-04-10 01:54:55 -0500

    more stuff added to ch 4

--- Languages/C++/a_PPAP-C++/ch4/exercise/redo/ex18.cpp
+++ Languages/C++/a_PPAP-C++/ch4/exercise/redo/ex18.cpp
@@ -18,6 +18,7 @@ output both x1 and x2.
 double solve_quadratic_pos(double, double, double);
 double solve_quadratic_neg(double, double, double);
 double power_check(double, double, double);
+void get_results(double, double, double);
 
 int main()
 {
@@ -27,32 +28,12 @@ int main()
          << "This is the form ax^2 + bx + c \n"
          << "Enter the values for a b and c and I will tell you what x is\n";
 
-    cin >> a >> b >> c;
-
-    if (power_check(a, b, c) == 0)
-    {
-        cout << "One real root exists\n";
-        cout << "x: " << solve_quadratic_pos(a, b, c) << "\n";
-    }
-
-    if (power_check(a, b, c) > 0)
-    {
-        cout << "Two real roots exist: \n";
-        cout << "Positive factor x: " << solve_quadratic_pos(a, b, c) << "\n";
-        cout << "negative factor x: " << solve_quadratic_neg(a, b, c) << "\n";
-    }
-
-    if (power_check(a, b, c) < 0)
-    {
-        cout << "Two complex roots exist: \n";
-        cout << "Positive factor x: " << -b << " + sqrt(" << power_check(a,b,c)  
-             <<  ")\n";
-
-        cout << "negative factor x: " << -b << " - sqrt(" << power_check(a,b,c)  
-             <<  ")\n";
-    }
-
-
+    cout << "a: "; cin >> a;
+    cout << "b: "; cin >> b;
+    cout << "c: "; cin >> c;
+    
+    get_results(a,b,c);
+    
 }
 
 double solve_quadratic_pos(double a, double b, double c)
@@ -71,5 +52,31 @@ double solve_quadratic_neg(double a, double b, double c)
 
 double power_check(double a, double b, double c)
 {
-    return pow(b, 2) - (4*a*c);
+    return pow(b, 2) - (4 * a * c);
+}
+
+void get_results(double a, double b, double c)
+{
+    if (power_check(a, b, c) == 0)
+    {
+        cout << "One real root exists\n";
+        cout << "x: " << solve_quadratic_pos(a, b, c) << "\n";
+    }
+
+    if (power_check(a, b, c) > 0)
+    {
+        cout << "Two real roots exist: \n";
+        cout << "Positive factor x: " << solve_quadratic_pos(a, b, c) << "\n";
+        cout << "negative factor x: " << solve_quadratic_neg(a, b, c) << "\n";
+    }
+
+    if (power_check(a, b, c) < 0)
+    {
+        cout << "Two complex roots exist: \n";
+        cout << "Positive factor x: " << -b << " + sqrt(" << power_check(a, b, c)
+             << ")\n";
+
+        cout << "negative factor x: " << -b << " - sqrt(" << power_check(a, b, c)
+             << ")\n";
+    }
 }
